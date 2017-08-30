4ded3e3e60e91a0448a247ecba5c8e6d3a2273cc
szy0syz <szy0syz@gmail.com>
2017-08-30 17:06:14 +0800

    updated lesson10

---
+++ lesson11_file/rwDemo.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+
+// 0  stdin
+// 1  stdout
+// 2  stderr
+
+setTimeout(function () {
+  var buffer = new Buffer(1);
+  console.log(buffer);
+  // 用fs读stdin
+  fs.read(0, buffer, 0, 1, 0, function (err) {
+    console.log(buffer);
+  })
+}, 3000);
+
+process.stdin.on('data', function (err, data) {
+  console.log(arguments);
+});
+
+setInterval(function () { }, 1000);
