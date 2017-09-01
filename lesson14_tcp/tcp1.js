d4b847512548cac5168f6dd1ee22069505b5ddb6
szy0syz <szy0syz@gmail.com>
2017-09-01 15:56:33 +0800

    updated lesson14_demo

---
+++ lesson14_tcp/tcp1.js
@@ -0,0 +1,22 @@
+var net = require('net');
+var util = require('util');
+
+var server = net.createServer(function(socket){
+  console.log(util.inspect(socket.address()));
+  // 查看当前连接数量
+  server.getConnections(function(err, count) {
+    console.log('TCPs:' ,count);
+  });
+  socket.on('error', function(err) {
+    console.log(err);
+    socket.destroy();
+  })
+});
+
+server.on('error', function(err) {
+  console.log(err);
+});
+
+server.listen(8088, function() {
+  console.log(util.inspect(server.address()));
+});
