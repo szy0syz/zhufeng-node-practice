31cac1c02ccc476201e82daa5a8fb4b255fa3742
szy0syz <szy0syz@gmail.com>
2017-09-02 14:30:23 +0800

    updated lesson15

--- lesson15_tcp2/chatServ.js
+++ lesson15_tcp2/chatServ.js
@@ -12,14 +12,19 @@ var clients = {};
 var server = net.createServer(function (socket) {
   var nickname;
   socket.setEncoding('utf8');
+  // 异步方式回去服务器在线人数和欢迎语
   server.getConnections(function (err, count) {
     socket.write('欢迎光临，当前共在线' + count + '，请输入用户名：\r\n> ');
   });
 
+  // 监听socket上可读流方向上的data事件，如果读到来自客户端数据就触发事件 
   socket.on('data', function (data) {
+    // 过滤回车换行
     data = data.replace(/\r\n/, '');
+    // 如果在当前闭包里，nickname已经有值就说明设置过名称，直接将他输入的内容广播出去
     if (nickname) {
       broadcast(nickname, nickname + ': ' + data);
+    // 否则就认为是新用户登录，则设置名称、存储socket、广播新用户到来
     } else {
       nickname = data;
       clients[nickname] = socket;
@@ -27,9 +32,11 @@ var server = net.createServer(function (socket) {
     }
   });
 
+  // 监听end，在流程中end比close先触发
   socket.on('end', function (data) {
     console.log('client is end...');
     broadcast(nickname, '[系统广播]' + nickname + '离开了聊天室! ');
+    // 清除socket
     if (clients[nickname]) {
       clients[nickname].destroy();
       delete clients[nickname];
@@ -40,6 +47,7 @@ var server = net.createServer(function (socket) {
     console.log(err);
   });
 
+  // 监听close事件，在end事件后触发，再次确认清除一下客户端的socket
   socket.on('close', function () {
     console.log('client is closed...');
     if (clients[nickname]) {
@@ -47,9 +55,9 @@ var server = net.createServer(function (socket) {
       delete clients[nickname];
     }
   });
-
 });
 
+// 广播函数
 function broadcast(nickname, msg) {
   for (var name in clients) {
     if (nickname !== name) {
@@ -58,6 +66,7 @@ function broadcast(nickname, msg) {
   }
 }
 
+// 开启 服务器
 server.listen(8090, function () {
   console.log('Chat-server is running...');
 })
