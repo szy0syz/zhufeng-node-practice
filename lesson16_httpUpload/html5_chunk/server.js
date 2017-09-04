abc9b6338051391b2fa04dabcfee6c84a61ae285
szy0syz <szy0syz@gmail.com>
2017-09-04 23:18:25 +0800

    updated lesson16 分片传输后端完成，哈哈！

--- lesson16_httpUpload/html5_chunk/server.js
+++ lesson16_httpUpload/html5_chunk/server.js
@@ -7,6 +7,7 @@ var url = require('url');
 var fs = require('fs');
 var path = require('path');
 process.chdir(__dirname);
+var renameCount = 0;
 var app = http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true); // 确定转换为对象方式
   var pathname = urlObj.pathname;
@@ -20,18 +21,34 @@ var app = http.createServer(function (req, res) {
       }
       var file = files.data;
       var filename = fields.name;
-      var total = fields.total;
+      var total = Number(fields.total);
       var index = fields.index;
       var size = fields.size;
+      var tempDir = path.dirname(file.path);
+
       // 教程里这里创建可读流写一个新文件，太费性能了，改名就好了嘛
       // var src = fs.createReadStream()
       // 同步还是异步呢
-      fs.renameSync(file.path, file.path.replace(path.basename(file.path), filename + '.' + index));
-      //fs.rename(file.path, file.path.replace(path.basename(file.path), filename + '.' + index));
-      console.log(file.path);
-
-
-
+      // fs.renameSync(file.path, file.path.replace(path.basename(file.path), filename + '.' + index));
+      fs.rename(file.path, file.path.replace(path.basename(file.path), filename + '.' + index), function () {
+        if (renameCount === total - 1) {
+          var fd = fs.openSync(filename, 'a');
+          var files = fs.readdirSync(tempDir);
+          files.forEach(function (item, idx) {
+            if (item.startsWith(filename + '.')) {
+              var pos = Number(path.extname(item).slice(1)) * size; // 不需要-1
+              fs.readFile(tempDir + '/' + item, function (err, buff) {
+                fs.writeSync(fd, buff, 0, buff.length, pos);
+                fs.unlinkSync(tempDir + '/' + item);
+                renameCount = 0;
+              });
+            }
+          })
+        } else {
+          renameCount++;
+        }
+      });
+      
       res.end('file ok');
     })
   } else {
