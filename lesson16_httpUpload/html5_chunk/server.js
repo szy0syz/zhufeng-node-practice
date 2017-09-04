25f91b44bae907d128fa2ebba69b46b6e337c594
szy0syz <szy0syz@gmail.com>
2017-09-04 17:06:49 +0800

    updated lesson16

--- lesson16_httpUpload/html5_chunk/server.js
+++ lesson16_httpUpload/html5_chunk/server.js
@@ -3,29 +3,41 @@ var http = require('http');
 var util = require('util');
 var url = require('url');
 var formidable = require('formidable');
-// var mine = require('mine');
 var url = require('url');
 var fs = require('fs');
+var path = require('path');
 process.chdir(__dirname);
-var app = http.createServer(function(req, res) {
+var app = http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true); // 确定转换为对象方式
   var pathname = urlObj.pathname;
-  if(pathname === '/') {
+  if (pathname === '/') {
     fs.createReadStream('./index.html').pipe(res);
   } else if (pathname === '/post') {
     var parse = new formidable.IncomingForm();
-    parse.parse(req, function(err, fields, files) {
-      if(err) {
+    parse.parse(req, function (err, fields, files) {
+      if (err) {
         res.end('file error');
       }
       var file = files.fileUpload;
-      console.log(util.inspect(file));
+      var filename = files.name;
+      var total = files.total;
+      var index = files.index;
+      var size = files.size;
+      // 教程里又读写一个新文件，太费性能了，改名就好了
+      // var src = fs.createReadStream()
+      console.log(path.basename(file.path));
+      fs.renameSync(file.path)
+
+
+
+
+
       res.end('file ok');
     })
   } else {
     res.end('404');
   }
 
-}).listen(8088, function() {
+}).listen(8088, function () {
   console.log('server is running...');
 });
