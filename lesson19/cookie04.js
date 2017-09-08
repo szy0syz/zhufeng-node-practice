1ac85ab1d7fc933186a4c3bb6085d29e119b5150
szy0syz <szy0syz@gmail.com>
2017-09-08 23:46:16 +0800

    finished lesson19

---
+++ lesson19/cookie04.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var app = express();
+
+app.use(cookieParser('jjjkkk'));
+app.set('view engine', 'html');
+app.engine('html', require('ejs').__express);
+app.set('views', __dirname);
+
+app.get('/', function (req, res) {
+  res.render('index', { title: '请登录' });
+});
+
+app.get('/login', function (req, res) {
+  res.cookie('username', req.query.username, { signed: true });
+  res.cookie('isLogin', 1);
+  res.redirect('/user');
+});
+
+app.get('/user', function (req, res) {
+  if (req.cookies.isLogin === '1') {
+    res.end(req.signedCookies.username);
+  } else {
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    res.end();
+  }
+});
+
+app.listen(8080);
