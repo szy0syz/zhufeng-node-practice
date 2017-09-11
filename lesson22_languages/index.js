c6ba11c9dcafd162ab93ccb8ba176a293cc0dfa2
szy0syz <szy0syz@gmail.com>
2017-09-11 16:01:11 +0800

    updated lesson20注释

--- lesson22_languages/index.js
+++ lesson22_languages/index.js
@@ -14,21 +14,21 @@ function checkLanguage(languages) {
       .split(',')
       .map(function (lan) {
         var parts = lan.split(';');
-        return { name: parts[0], q: parts[1] || 1 }
+        return { name: parts[0], q: parts[1] || 1 }  // 转换成对象数组
       })
-      .filter(function (lang) {
+      .filter(function (lang) {  // 过滤掉服务器端不能提供的语言
         return languages.indexOf(lang.name) !== -1;
       })
       .sort(function (prev, next) {
-        return next.q - prev.q;
+        return next.q - prev.q;  //按q权重来排序，从高到低
       })
       .map(function (item) {
-        return item.name;
+        return item.name;  // 把数组的每一个元素转成字符串
       })
   }
-  return function(req, res, next) {
+  return function (req, res, next) {
     var acceptLanguages = req.headers['accept-language'];
-    console.log('accept-language:',acceptLanguages);
+    console.log('accept-language:', acceptLanguages);
     req.acceptLanguages = parse(acceptLanguages)[0] || languages[0];
     next();
   }
@@ -38,7 +38,7 @@ app.use(checkLanguage(['zh-CN', 'en']));
 
 app.get('/', function (req, res) {
   res.setHeader('Content-Language', req.acceptLanguages);
-  res.sendFile(path.join(__dirname,req.acceptLanguages, 'index.html'));
+  res.sendFile(path.join(__dirname, req.acceptLanguages, 'index.html'));
 })
 
 app.listen(8080);
