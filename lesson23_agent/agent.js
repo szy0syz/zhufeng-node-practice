d2d5cc37ebf4fa7974f24e8480e8fac663c8dd5b
szy0syz <szy0syz@gmail.com>
2017-09-11 22:47:28 +0800

    finished lesson23-2

--- lesson23_agent/agent.js
+++ lesson23_agent/agent.js
@@ -1,24 +1,37 @@
 var express = require('express');
 var path = require('path');
-var agentParser = require('user-agent-parser');
-var app = express();
-var visit = {mobile:0,other:0};
+var proxy = require('http-proxy').createProxyServer();
 
 // "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36"
 
-app.use(function(req,res,next){
-   req.agent = agentParser(req.headers['user-agent']||'');
-    next();
-});
+var app = express();
 
-app.get('/',function(req,res){
-    console.log(req.agent);
-   if(req.agent.device.type == 'mobile'){
-       visit.mobile = visit.mobile+1;
-   }else{
-       visit.other = visit.other+1;
-   }
-   res.send(visit);
-});
+function proxyPass(config) {
+    return function (req, res) {
+        var target = config[req.hostname];
+        proxy.web(req, res, {
+            target: target
+        });
+    }
+}
+
+app.use(proxyPass({
+    'a.szy.com': 'http://localhost:3000',
+    'b.szy.com': 'http://localhost:4000'
+}));
 
 app.listen(8080);
+
+// a.szy.com
+var app3000 = express();
+app3000.get('/', function (req, res) {
+    res.end('3000');
+});
+app3000.listen(3000);
+
+// b.szy.com
+var app4000 = express();
+app4000.get('/', function (req, res) {
+    res.end('4000');
+});
+app4000.listen(4000);
