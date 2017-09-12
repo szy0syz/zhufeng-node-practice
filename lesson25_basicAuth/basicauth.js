3b2f8224d21e91e58feab70deb0991c1169f8894
szy0syz <szy0syz@gmail.com>
2017-09-12 17:04:59 +0800

    updated lesson25-basicAuth

---
+++ lesson25_basicAuth/basicauth.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+http.createServer(function(req, res) {
+  var auth = req.headers['authorization'];
+  if (auth) {
+    var area = auth.slice(6);
+    var parts = new Buffer(area, 'base64').toString().split(':');
+    if(parts[0] + parts[1] === 'admin123') {
+      res.end('welcome');
+    }
+  } else {
+    res.setHeader('WWW-Authenticate', 'Basic realm="secure Area"');
+    res.writeHead(401);
+    return res.end();
+  }
+}).listen(8080);
