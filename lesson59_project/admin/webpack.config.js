97726820263fb485659dcb49bd18c1f749502156
szy0syz <szy0syz@gmail.com>
2017-09-17 23:01:21 +0800

    updated lesson59 webpack解析配置

--- lesson59_project/admin/webpack.config.js
+++ lesson59_project/admin/webpack.config.js
@@ -6,5 +6,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  resolve: { // 解析配置
+    root: ['./src'], //设置项目webpack解析的起始根据路为src文件夹开始,如果webpack找模块只会去src里去找
+    extensions: ['', 'js', 'css'] //webpack后缀解析，如果没加js或css会自动识别
+
+
   }
 }
